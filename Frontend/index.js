04390ea421e05a4a0fd5a93e1724f405886117a3
yoseflehrer <yolehrer@gmail.com>
2019-08-21 09:13:52 -0400

    Added reload to gameover card

--- Frontend/index.js
+++ Frontend/index.js
@@ -84,15 +84,9 @@ draw(0);
 
 // Get the modal
 const modal = document.getElementById("myModal");
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
 const scoreHolder = document.getElementById("score-holder")
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-};
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
